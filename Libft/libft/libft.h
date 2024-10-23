ac1f7fd1d440be397b77d9e567aee146d524ac59
Aimane Yousr <ayousr@c1r4p8.1337.ma>
2024-10-23 20:40:49 +0100

    commit

---
+++ Libft/libft/libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayousr <ayousr@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 18:59:32 by ayousr            #+#    #+#             */
+/*   Updated: 2024/10/23 19:22:56 by ayousr           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(const int n);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(char c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+
+#endif 
--- libft/ft_isalnum.c
+++
@@ -1,4 +0,0 @@
-int isalnum(int c)
-{
-    return (ft_isalpha(c) || ft_isdigit(c));
-}
