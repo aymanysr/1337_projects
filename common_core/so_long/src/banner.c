faf6bce9e387ed5c951ab33a6fb58ccf9d61cfc1
aymanysr <aimaneyousr@gmail.com>
2025-02-13 16:11:53 +0100

    Add game banner display function

---
+++ common_core/so_long/src/banner.c
@@ -0,0 +1,16 @@
+#include "so_long.h"
+
+void    game_banner(void)
+{
+	ft_printf("%s--------------------------------------- \n", G);
+	ft_printf("%s| By -> Aimane Yousr (ayousr) |\n", G);
+	ft_printf("%s--------------------------------------- \n", G);
+	ft_printf("%s ____           _                       \n", Y);
+	ft_printf("%s/ ___|  ___    | |    ___  _ __   __ _  \n", R);
+	ft_printf("%s/___ / / _ /   | |   / _ /| '_ / / _` | \n", G);
+	ft_printf("%s ___) | (_) |  | |__| (_) | | | | (_| | \n", B);
+	ft_printf("%s|____/ /___/___|_____/___/|_| |_|/__, | \n", R);
+	ft_printf("%s--------------------------------------- \n", G);
+	ft_printf("%s|          Have a good trip :)         |\n", G);
+	ft_printf("%s--------------------------------------- \n", G);
+}
