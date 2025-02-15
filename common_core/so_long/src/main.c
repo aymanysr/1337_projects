ad2e4f1466c32e58ba80fb879d86d43b208c9390
aymanysr <aimaneyousr@gmail.com>
2025-02-15 18:25:34 +0100

    Refactor main function to improve argument handling and game initialization flow

--- common_core/so_long/src/main.c
+++ common_core/so_long/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayousr <ayousr@student.42.fr>              +#+  +:+       +#+        */
+/*   By: aimaneyousr <aimaneyousr@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 13:26:32 by ayousr            #+#    #+#             */
-/*   Updated: 2025/02/05 18:09:03 by ayousr           ###   ########.fr       */
+/*   Updated: 2025/02/15 18:24:27 by aimaneyousr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,17 @@ int	main(int ac, char **av)
 	}
 	else if (ac == 2 && !(is_ber_file(av[1])))
 		ft_printf("ERROR\nUsage: %s <map.ber>\n", av[0]);
-	game.map_data = *parse_map("maps/map1.ber");
-	game.mlx = mlx_init(game.map_data->width * PIXELS,
-			game.map_data->height * PIXELS, "so_long", false);
-	mlx_loop(game.mlx);
+	else if (ac == 2)
+	{
+		game.map_path = av[1];
+		vars_initializer(&game);
+		if (components_checker(&game))
+		{
+			game_banner();
+			mlx_initializer(&game);
+			game_renderer(&game);
+		}
+	}
+	ft_game_errors("[!!!] Invalid arguments");
+	return (0);
 }
