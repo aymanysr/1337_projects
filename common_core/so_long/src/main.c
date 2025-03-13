eacf3a8f3b484fe5f29c78f747da683b1eec6409
aymanysr <aimaneyousr@gmail.com>
2025-03-13 21:16:27 +0000

    nomineete

--- common_core/so_long/src/main.c
+++ common_core/so_long/src/main.c
@@ -6,7 +6,7 @@
 /*   By: aimaneyousr <aimaneyousr@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 13:26:32 by ayousr            #+#    #+#             */
-/*   Updated: 2025/03/11 13:38:06 by aimaneyousr      ###   ########.fr       */
+/*   Updated: 2025/03/13 21:16:04 by aimaneyousr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,21 @@ int	main(int ac, char **av)
 {
 	t_game	*game;
 
-	/* Immediately handle invalid arguments */
 	if (ac != 2)
 		ft_game_errors("Usage: ./so_long <map.ber>");
-	
-	/* Allocate and initialize game structure */
 	game = malloc(sizeof(t_game));
 	if (!game)
 		ft_game_errors("Memory allocation error");
 	vars_initializer(game);
 	vars_initializer_bonus(game);
 	game->map_path = av[1];
-
-	/* Validate map and its components */
 	if (!components_checker(game))
 		ft_game_errors("[!!!] Map validation failed.");
-
-	/* Initialize MLX, load assets, and display initial game state */
 	mlx_initializer(game);
 	game_banner();
 	load_enemy_textures(game);
 	init_enemies(game);
 	initialize_game(game);
-	
-
-	/* Set up key and loop hooks */
 	mlx_key_hook(game->mlx.mlx_ptr, player_moover, game);
 	mlx_loop_hook(game->mlx.mlx_ptr, update_game, game);
 	mlx_loop(game->mlx.mlx_ptr);
